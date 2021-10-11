3c88db70faa7b6d3186dcba6d059dbecf0a4654a
Tejassonar <sonartejas908@gmail.com>
2021-10-11 12:02:18 +0530

    Contains main method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day20_Ms_Test_UserRegistration
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
